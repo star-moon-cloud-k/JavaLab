af31fa0b08a9e142165affdb26bbfe94e7670549
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-06 18:42:49 +0900

    foreach

--- JavaLab/src/intermediate/BitNShiftMain.java
+++ JavaLab/src/intermediate/BitNShiftMain.java
@@ -1,7 +1,7 @@
 package intermediate;
 
 public class BitNShiftMain {
-    public static String shifts(int a) {
+    public static String shifts1(int a) {
         String s = "";
         for (int i = 0; i <= 31; i++) {
             int aa = a % 2;
@@ -10,11 +10,38 @@ public class BitNShiftMain {
         }
         return s;
     }
+
+    public static int BITMASK = 1;
+    public static String shifts2(int a){
+        int BITMASK =1;
+        String s ="";
+        for(int i =0; i<= 31; i++){
+            s = (a & BITMASK) + s;
+            a >>=1;        //a /= 2;
+        }
+        return s;
+    }
+
+    public static String shifts3(int a){
+        int BITMASK =1;
+        String s ="";
+        for(int i =0; i<= 31; i++){
+            s = (a & BITMASK) + s;
+            a >>=1;        //a /= 2;
+        }
+        return s.substring(s.indexOf('1'));
+    }
         public static void main(String[] args){
             int intNums = 123;
             int intNums2= -123;
-            System.out.printf("%d : %s %n", intNums, shifts(intNums));
-            System.out.printf("%d : %s %n", intNums2, shifts(intNums2));
+            System.out.printf("shifts1 %d : %s %n", intNums, shifts1(intNums));
+            System.out.printf("shifts1 %d : %s %n", intNums2, shifts1(intNums2));
+
+            System.out.printf("shifts2 %d : %s %n", intNums, shifts2(intNums));
+            System.out.printf("shifts2 %d : %s %n", intNums2, shifts2(intNums2));
+
+            System.out.printf("shifts3 %d : %s %n", intNums, shifts3(intNums));
+            System.out.printf("shifts3 %d : %s %n", intNums2, shifts3(intNums2));
         }
 
 }
---
+++ JavaLab/src/intermediate/ForeachLotto.java
@@ -0,0 +1,11 @@
+package intermediate;
+
+public class ForeachLotto {
+    public static void main(String[] args){
+        int[] mm = {1, 2, 16, 22, 23, 33};
+        for (int m : mm){
+            System.out.printf(m + "\t");
+        }
+        System.out.println();
+    }
+}
