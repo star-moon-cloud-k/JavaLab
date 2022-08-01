70b20a252097dbcb2639634a776455061fafa368
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-01 09:16:38 +0900

    factory pattern

---
+++ JavaLab/src/DesignPattern/FactoryPattern/Pizza.java
@@ -0,0 +1,15 @@
+package DesignPattern.FactoryPattern;
+
+public class Pizza {
+    public void prepare() {
+    }
+
+    public void bake() {
+    }
+
+    public void cut() {
+    }
+
+    public void box() {
+    }
+}
---
+++ JavaLab/src/DesignPattern/FactoryPattern/SimplePizzaFactory.java
@@ -0,0 +1,6 @@
+package DesignPattern.FactoryPattern;
+
+public class SimplePizzaFactory {
+    public Pizza createPizza(String type) {
+    }
+}
